2b2da0844f89071c740fb14628c7007fc7442253
reedanna <reedanna@mail.com>
2020-07-21 09:02:17 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,50 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    self.paintings.map do |painting|
+      painting.gallery
+    end.uniq
+  end
+
+  def cities
+    self.galleries.map do |gallery|
+      gallery.city
+    end.uniq
+  end
+
+  def self.total_experience
+    self.all.map do |artist|
+      artist.years_experience
+    end.sum
+  end
+
+  def self.most_prolific
+    self.all.max do |artista, artistb|
+      (artista.paintings.count.to_f/artista.years_experience.to_f) <=> (artistb.paintings.count.to_f/artistb.years_experience.to_f)
+    end
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(self, title, price, gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,13 @@
 require_relative '../config/environment.rb'
 
+anna = Artist.new("Anna Reed", 15)
+picasso = Artist.new("Pablo Picasso", 200)
 
+louvre = Gallery.new("The Louvre", "Paris")
+guggenheim = Gallery.new("Guggenheim", "New York")
 
+still_life = Painting.new(anna, "Still Life", 200000, louvre)
+portrait = Painting.new(picasso, "Portrait", 1000000, louvre)
 
 binding.pry
 
