0c7ed6399d3a6f473ed4becea7b8727723748e5f
allenorton <norton.allene@gmail.com>
2020-07-20 21:06:37 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,36 @@
 class Artist
-
   attr_reader :name, :years_experience
-
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    self.class.all << self 
+  end
+  def self.all
+    @@all 
+  end
+  
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, gallery, self)
+  end 
+
+  def paintings
+    Painting.all.select {|p| p.artist == self}
   end
 
-end
+  def galleries
+    paintings.map {|p| p.gallery}
+  end
+
+  def cities
+    galleries.map {|g| g.city}
+  end
+
+  def self.total_experience
+    self.all.sum {|a| a.years_experience}
+  end
+
+  def self.most_prolific
+    self.all.max_by {|artist| artist.paintings.length / artist.years_experience}
+  end 
+end 
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,32 @@
 class Gallery
 
   attr_reader :name, :city
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    self.class.all << self 
+  end
+  
+  def self.all
+    @@all
+  end 
+
+  def paintings
+    Painting.all.select {|paintings| paintings.gallery == self}
+  end 
+
+  def artists
+    paintings.map {|p| p.artist}
   end
 
+  def artist_names
+    paintings.map {|p| p.artist.name}
+  end
+
+
+  def most_expensive_painting
+    Painting.all.max {|a, b| a.price <=> b.price}
+  end
 end
